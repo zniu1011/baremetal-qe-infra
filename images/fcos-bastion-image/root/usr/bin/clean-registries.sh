ab9351756fe660865aa618bb4c9724dab016b729
zniu <zniu@redhat.com>
2025-11-28 18:33:04 +0800

    Move registry tags cleanup step to post step

--- images/fcos-bastion-image/root/usr/bin/clean-registries.sh
+++ images/fcos-bastion-image/root/usr/bin/clean-registries.sh
@@ -21,8 +21,7 @@ wait_for_container_running() {
 mapfile -t ports < <(systemctl list-units 'registry@*' --no-pager --quiet | awk -F'[@.]' '{print $2}')
 for port in "${ports[@]}"; do
   disk_use=$(df /opt/registry-"${port}" --output='pcent' | grep -o '[0-9]*')
-  if [ "$disk_use" -gt 85 ]; then
-    rm -rf /opt/registry-"${port}"/data/docker/registry/v2/repositories/*
+  if [ "$disk_use" -gt 70 ]; then
     sed -i -e '/readonly/{n;s/enabled:.*/enabled: true/;}' /opt/registry-"${port}"/config.yaml
     systemctl restart registry@"${port}".service
     wait_for_container_running registry-"${port}"
